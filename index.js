30e6fb645ca4e503c5bba8540b1847f86dbed62a
Savitha <savitha.gollamudi@gmail.com>
2020-07-07 14:09:31 +0530

    final version of app

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const { hash } = window.location;
+const message = atob(hash.replace("#", ""));
+if (message) {
+  document.querySelector("#message-card").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+  document.querySelector("h1").innerHTML = message;
+}
+
 document.querySelector("form").addEventListener("submit", (event) => {
   //prevents default behaviour of form to submit to a server
   event.preventDefault();
 
   const msgIp = document.querySelector("#message-input");
   const linkIp = document.querySelector("#link-input");
+  document.querySelector("#message-card").classList.add("hide");
+  document.querySelector("#link-card").classList.remove("hide");
   //convert the input value(ascii) to base64 encoding
   const encrypted = btoa(msgIp.value);
   //current url
